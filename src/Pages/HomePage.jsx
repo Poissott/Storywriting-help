10dc4bde09517a2091ff10ec026eee6c56f830a7
Poissott <ottpoiss8@gmail.com>
2025-07-05 17:17:48 +0300

    Random three words

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import playGame from "./PlayGame.jsx";
 import {Link} from "react-router-dom";
 
 function HomePage() {
     return (
-        <body className="bg-one ">
-        <div className="min-h-screen min-w-screen content-center">
-            <div className="bg-two min-h-100 min-w-200 ml-120 mr-120 rounded-md">
-                <div className="container flex flex-col justify-center items-center min-h-100 p-7 gap-5">
-                    <text className="text-5xl text-four text-center">This is Taletelling</text>
-                    <Link to="/game">
-                        <button onClick={playGame} className="bg-three min-h-15 min-w-40 object-center rounded-md">Continue</button>
-                    </Link>
+        <div className="bg-one ">
+            <div className="min-h-screen min-w-screen content-center">
+                <div className="bg-two min-h-100 min-w-200 ml-120 mr-120 rounded-md">
+                    <div className="container flex flex-col justify-center items-center min-h-100 p-7 gap-5">
+                        <p className="text-5xl text-four text-center">This is Taletelling</p>
+                        <Link to="/game">
+                            <button className="bg-three min-h-15 min-w-40 object-center rounded-md">Continue</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
-        </body>
     );
 }
 
--- src/Pages/PlayGame.jsx
+++ src/Pages/PlayGame.jsx
@@ -1,40 +1,52 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import wordsTxt from "/wordlist.txt";
 import {Link} from "react-router-dom";
 import {useQuery} from "@tanstack/react-query";
 
 function PlayGame() {
     const [word, setWord] = useState({word1: "", word2: "", word3: ""});
+
     const getWords = async () => {
-        const words = await fetch(wordsTxt);
-        console.log(words.text());
-        return await words.text();
+        const text = await fetch(wordsTxt);
+        const words = await text.text();
+        return words.split("\r\n")
     }
-    const [data] = useQuery({
+
+    const {data} = useQuery({
+        queryKey: ["wordsTxt"],
         queryFn: getWords,
-        queryKey: "wordsTxt",
-        staleTime: Infinity,
+        staleTime: Infinity
     })
 
-    pickRandom(data);
+    useEffect(() => {
+        if (Array.isArray(data)) {
+            const threeRandom = (data) => {
+                const three = [...data];
+                for (let i = three.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [three[i], three[j]] = [three[j], three[i]];
+                }
+                return three.slice(0, 3);
+            }
+            const [word1, word2, word3] = threeRandom(data);
+            setWord({word1, word2, word3});
+        }
+    }, [data])
 
-    function pickRandom(wordArr) {
-        const words = {...word, word1: wordArr[0], word2: wordArr[1], word3: wordArr[2]};
-        console.log(words);
-        setWord(words);
-    }
-
-    return (
-        <body  className="bg-one ">
+    return (<div className="bg-one ">
         <div className="min-h-screen min-w-screen content-center">
             <div className="bg-two min-h-100 min-w-200 ml-120 mr-120 rounded-md">
                 <div className="container flex flex-col justify-center items-center min-h-100 p-7 gap-5">
-                    <text className="text-5xl text-four text-center">Your given words: {word.word1}, {word.word2}, {word.word3}</text>
+                    <p className="text-5xl text-four text-center">Your given
+                        words: {word.word1}, {word.word2}, {word.word3}</p>
+                    <div className="h-150 w-127 bg-one  border-one rounded-md flex flex-col justify-center items-center p-2">
+                        <textarea className=" min-h-123 min-w-110 bg-two justify-center items-center text-amber-50 m-2"></textarea>
+                        <button className="bg-three h-15 min-h-15 w-40 justify-center items-center rounded-md m-2">Submit</button>
+                    </div>
                 </div>
             </div>
         </div>
-        </body>
-    );
+    </div>);
 }
 
-export default PlayGame;
+export default PlayGame;    
